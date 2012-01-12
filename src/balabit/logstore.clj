f333e040914b1d9c19e086ba953e5cde51eceb2f
Gergely Nagy <algernon@balabit.hu>
2012-01-12 11:41:08 +0100

    balabit.logstore: Implement with-logstore-record and other helpers.
    Adds a with-logstore-record macro, and a couple of other helpers to
    make it more convenient to work with records.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- src/balabit/logstore.clj
+++ src/balabit/logstore.clj
@@ -2,9 +2,11 @@
   "A convenient API for reading syslog-ng PE LogStore files."
 
   (:require [balabit.logstore.core :as lgs])
-  (:import balabit.logstore.core.LSTFile))
+  (:import balabit.logstore.core.LSTFile)
+  (:import balabit.logstore.record.LSTRecord))
 
 (declare ^:dynamic ^LSTFile *logstore*)
+(declare ^:dynamic ^LSTRecord *logstore-record*)
 
 (defmacro with-logstore
   "Do stuff with a LogStore. The macro keeps the LogStore file opened,
@@ -17,3 +19,25 @@
   "Returns the header of the currently opened LogStore file."
   [& fields]
   `(reduce get (:header *logstore*) [~@fields]))
+
+(defmacro logstore-records
+  "Returns the record headers in an opened LogStore file"
+  []
+  `(:record-map *logstore*))
+
+(defmacro logstore-nth
+  "Returns the Nth record from an opened LogStore file"
+  [index]
+  `(lgs/nth *logstore* ~index))
+
+(defmacro with-logstore-record
+  "Do stuff with a specific LogStore record"
+  [index & body]
+  `(binding [*logstore-record* (logstore-nth ~index)]
+     (do ~@body)))
+
+(defmacro logstore-record
+  "Returns the current LogStore record, or optionally, a field of
+  it."
+  [& fields]
+  `(reduce get *logstore-record* [~@fields]))
--- test/balabit/logstore/test/logstore.clj
+++ test/balabit/logstore/test/logstore.clj
@@ -7,4 +7,15 @@
          (logstore-header :magic) => "LST4"
          (logstore-header :length) => 183
          (logstore-header :crypto :algo_hash) => "SHA1"
-         (logstore-header :crypto :algo_crypt) => "AES-128-CBC"))
+         (logstore-header :crypto :algo_crypt) => "AES-128-CBC")
+
+  (fact "about the number of records in the logstore"
+        (count (logstore-records)) => 9)
+
+  (with-logstore-record 1
+    (facts "about a specific record within the logstore"
+           (logstore-record :header :offset) => 13878
+           (logstore-record :header :size) => 14079
+           (logstore-record :header :type) => :chunk
+           (logstore-record :header :flags) => [:compressed]
+           (.limit (logstore-record :data)) => 14073)))
