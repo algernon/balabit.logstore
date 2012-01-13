0e43b59288dce99c70869c3a656a71aa1e765f4c
Gergely Nagy <algernon@balabit.hu>
2012-01-13 15:09:23 +0100

    tests: Remove some unneeded tests.
    The bulk of the library is tested by b.l.t.logstore, there's no need
    to perform the same tests on the low-level, private interface.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- test/balabit/logstore/test/core.clj
+++ test/balabit/logstore/test/core.clj
@@ -23,31 +23,3 @@
 
 (fact "about a non-existing logstore throwing an exception"
       (open-non-existant) => true)
-
-(def loggen-store (lst/open "resources/loggen.store"))
-
-(facts "about logstore meta-data"
-       (:magic (:header loggen-store)) => "LST4"
-       (:length (:header loggen-store)) => 183
-       (:algo_hash (:crypto (:header loggen-store))) => "SHA1"
-       (:algo_crypt (:crypto (:header loggen-store))) => "AES-128-CBC")
-
-(fact "about the number of records in the LogStore"
-      (lst/count loggen-store) => 9)
-
-(fact "about a record read from a LogStore"
-       (lst/nth loggen-store 1) =not=> nil)
-
-(def loggen-store-record (lst/nth loggen-store 1))
-
-(facts "about a record read from a LogStore"
-       (:offset (:header loggen-store-record)) => 13878
-       (:size (:header loggen-store-record)) => 14079
-       (:type (:header loggen-store-record)) => :chunk
-       (:flags (:header loggen-store-record)) => [:compressed]
-       (.limit (:data loggen-store-record)) => 13979
-       (:first_msgid loggen-store-record) => 4162
-       (:last_msgid loggen-store-record) => 8322
-       (:chunk_id loggen-store-record) => 1
-       (:xfrm_offset loggen-store-record) => 0
-       (:flags loggen-store-record) => [:hash])
