90f94882432eb08350bf095887d7b28dddae1a6d
Gergely Nagy <algernon@balabit.hu>
2012-03-27 14:46:12 +0200

    project.clj: Use an alpha version of gloss.
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/clojure "1.3.0"]
                  [slingshot "0.10.1"]
                  [joda-time/joda-time "2.0"]
-                 [gloss "0.2.1-SNAPSHOT"]
+                 [gloss "0.2.1-alpha1"]
                  ]
   :dev-dependencies [
                      [lein-marginalia "0.7.0-SNAPSHOT"]
