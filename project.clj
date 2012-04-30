50dc97e6efd052cced50d20bbccef40640dc1b18
Gergely Nagy <algernon@balabit.hu>
2012-04-30 15:24:13 +0200

    Add Clojure 1.5 profile and a with-all-profiles alias.
    Just for the heck of it, test with clojure 1.5 snapshot too (and add
    Sonatype repositories for that to work).
    
    And for future comptibility, add a with-all-profiles alias to
    project.clj, so the travis.yml file won't have to be modified if more
    profiles get added in the future: all interesting profiles will be
    controlled from a single place now, the project.clj file.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- project.clj
+++ project.clj
@@ -17,7 +17,15 @@
                  [gloss "0.2.1-rc1"]]
   :profiles {:dev {:dependencies
                    [[midje "1.3.1" :exclusions [org.clojure/clojure]]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases {"with-all-profiles" ["with-profile" "dev:dev,1.4:dev,1.5"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :plugins [[lein-marginalia "0.7.0"]
             [lein-midje "2.0.0-SNAPSHOT"]
             [lein-clojars "0.8.0"]
