88e3fc3622e2c8b2b96ed7fd74e19196689a1665
Gergely Nagy <algernon@balabit.hu>
2013-01-08 12:44:42 +0100

    lgstool: Tests for the 'search' command
    These are comprehensive tests for the 'search' command, testing all
    predicates in multiple ways. This fixes #9.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- lgstool/test/balabit/logstore/test/cli.clj
+++ lgstool/test/balabit/logstore/test/cli.clj
@@ -108,6 +108,123 @@
           parsed (logstore/from-file lgs-fn)]
       (is (= inspected parsed)))))
 
+(deftest search
+  (let [lgs-fn "../logstore/resources/logstores/sha256.store"
+        o-cat (cli/lgstool-cat message-identity lgs-fn)]
+    (testing "the 'search' command"
+      (testing "without arguments"
+        (let [o-search (cli/lgstool-search message-identity lgs-fn)]
+          (is (= o-cat o-search))))
+
+      (testing "the 'tag' predicate"
+        (let [o-tag (cli/lgstool-search message-identity lgs-fn
+                                        "(tag :s_tcp)")]
+          (is (= o-cat o-tag)))
+
+        (let [o-tag (cli/lgstool-search message-identity lgs-fn
+                                        "(tag :no-such-tag)")]
+          (is (empty? o-tag))))
+
+      (testing "the 'severity' predicate"
+        (let [o-sev (cli/lgstool-search message-identity lgs-fn
+                                        "(severity :informational)")]
+          (is (= o-cat o-sev)))
+
+        (let [o-sev (cli/lgstool-search message-identity lgs-fn
+                                        "(severity :error)")]
+          (is (empty? o-sev))))
+
+      (testing "the 'facility' predicate"
+        (let [o-fac (cli/lgstool-search message-identity lgs-fn
+                                        "(facility :auth)")]
+          (is (= o-cat o-fac)))
+
+        (let [o-fac (cli/lgstool-search message-identity lgs-fn
+                                        "(facility :mail)")]
+          (is (empty? o-fac))))
+
+      (testing "the '===' predicate"
+        (let [o-match (cli/lgstool-search message-identity lgs-fn
+                                          "(=== :HOST_FROM \"localhost\")")]
+          (is (= (count o-match) 3)))
+
+        (let [o-match (cli/lgstool-search message-identity lgs-fn
+                                          "(=== :SOCKET :family :inet4)")]
+          (is (= (count o-match) 3)))
+
+        (let [o-match (cli/lgstool-search message-identity lgs-fn
+                                          "(=== :random-thingy :foobar)")]
+          (is (empty? o-match))))
+
+      (testing "the 're-match' / '?=' predicates"
+        (let [o-re-match (cli/lgstool-search message-identity lgs-fn
+                                             "(re-match #\"seq: [0-9]+\")")
+              o-re-alias (cli/lgstool-search message-identity lgs-fn
+                                             "(?= #\"seq: [0-9]+\")")]
+          (is (= (count o-re-match) 3))
+          (is (= o-re-match o-re-alias)))
+
+        (let [o-re-match (cli/lgstool-search message-identity lgs-fn
+                                             "(?= :SOURCE #\"^s_\")")]
+          (is (= (count o-re-match) 3)))
+
+        (let [o-re-match (cli/lgstool-search message-identity lgs-fn
+                                             "(?= :PROGRAM \"prg\")")]
+          (is (= (count o-re-match) 3)))
+
+        (let [o-re-match (cli/lgstool-search message-identity lgs-fn
+                                             "(?= :PROGRAM \"foobar\")")]
+          (is (empty? o-re-match))))
+
+      (testing "the 'date' related predicates"
+        (let [lgs-fn "../logstore/resources/logstores/timestamped.store"]
+
+          (testing "'before'"
+            (let [o-before-2011 (cli/lgstool-search message-identity lgs-fn
+                                                    "(before \"2011-11-16T05:09:42.161+01:00\")")
+                  o-before-2012 (cli/lgstool-search message-identity lgs-fn
+                                                    "(before \"2012-01-01\")")
+                  o-before-2010 (cli/lgstool-search message-identity lgs-fn
+                                                    "(before \"2010-01-01\")")]
+              (is (= (count o-before-2011) 1))
+              (is (= (count o-before-2012) 2))
+              (is (empty? o-before-2010))))
+
+          (testing "'after'"
+            (let [o-after-2011 (cli/lgstool-search message-identity lgs-fn
+                                                   "(after \"2011-11-16T05:09:42.160+01:00\")")
+                  o-after-2012 (cli/lgstool-search message-identity lgs-fn
+                                                   "(after \"2012-01-01\")")
+                  o-after-2010 (cli/lgstool-search message-identity lgs-fn
+                                                   "(after \"2010-01-01\")")]
+              (is (= (count o-after-2011) 1))
+              (is (= (count o-after-2010) 2))
+              (is (empty? o-after-2012))))
+
+          (testing "'on'"
+            (let [o-on-1 (cli/lgstool-search message-identity lgs-fn
+                                             "(on \"2011-11-16T05:09:42.161+01:00\")")
+                  o-on-2 (cli/lgstool-search message-identity lgs-fn
+                                             "(on \"2011-11-16T05:09:13.562+01:00\")")
+                  o-on-3 (cli/lgstool-search message-identity lgs-fn
+                                             "(on \"2012-01-01\")")]
+              (is (= (count o-on-1) 1))
+              (is (= (count o-on-2) 1))
+              (is (empty? o-on-3))
+              (is (not (= o-on-1 o-on-2)))))))
+
+      (testing "multiple predicates"
+        (let [o-multi (cli/lgstool-search message-identity lgs-fn
+                                          "(=== :random-thingy \"this\")"
+                                          "(tag :s_tcp)"
+                                          "(?= \"00[12],\")")
+              o-none (cli/lgstool-search message-identity lgs-fn
+                                         "(=== :random-thingy \"this\")"
+                                         "(tag :s_tcp)"
+                                         "(?= \"foobar\")")]
+          (is (= (count o-multi) 2))
+          (is (empty? o-none)))))))
+
 (deftest main
   (testing "the '-main' function"
     (is (= (with-out-str (cli/lgstool-help))
